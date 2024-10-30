0d25c7b3933f9f6b30fc1a68505e1b967de52740
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-10-30 15:23:05 -0600

    Linted files as they failed

--- tests/frontPageCanBeOpened.spec.js
+++ tests/frontPageCanBeOpened.spec.js
@@ -1,14 +1,14 @@
-const { test, describe, expect, beforeEach } = require("@playwright/test");
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 beforeEach(async ({ page }) => {
-  await page.waitForTimeout(1000);
-  await page.goto("/");
-});
+  await page.waitForTimeout(1000)
+  await page.goto('/')
+})
 
-describe("Front Page", () => {
-  test("front page can be opened", async ({ page }) => {
+describe('Front Page', () => {
+  test('front page can be opened', async ({ page }) => {
     //await page.goto('/')
-    await expect(page.getByText("Jorge Montes")).toBeVisible();
-    await expect(page.getByText("Javier Coronado")).toBeVisible();
-  });
-});
+    await expect(page.getByText('Jorge Montes')).toBeVisible()
+    await expect(page.getByText('Javier Coronado')).toBeVisible()
+  })
+})
