b7839133af94466720bc8e14cc12c91988b01ca9
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-11-02 06:40:36 -0600

    Modifying pipeline to test background index.js start

--- playwright.config.js
+++ playwright.config.js
@@ -16,9 +16,39 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
   ],
   webServer: {
-    command: 'npm start', // Start server command
+    command: 'node index.js', // Start server command
     port: 3001, // Port Playwright will wait on
     reuseExistingServer: !process.env.CI, // Only create new server in CI
     timeout: 120 * 1000, // Allow server startup time
